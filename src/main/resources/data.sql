ffcbdf35be611006a88e8b10efea7ce40388ff50
nsvali00 <nikolasvalina001@gmail.com>
2023-10-22 13:44:22 +0200

    Added modules for h2 database and configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE test
+(
+    id int primary key auto_increment,
+    naziv varchar(50),
+    opis varchar(255)
+);
+
+INSERT INTO test(naziv,opis) VALUES ( 'Iva', 'ne radi joj punjac' );
